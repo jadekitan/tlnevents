4b3e65e39c67de31df40d30e8b625842de03a40b
= <jadekitan23@gmail.com>
2024-10-12 08:52:45 +0100

    Installed + Configure Chakra UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/outfit";
 
-createRoot(document.getElementById('root')).render(
+import * as ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+const colors = {
+  primary: {
+    500: "#80BC00",
+  },
+  secondary: {
+    500: "#F7EC31",
+  },
+};
+
+const theme = extendTheme({
+  colors,
+  fonts: {
+    heading: `'Urbanist', Arial, sans-serif`,
+    body: `'Urbanist', Arial, sans-serif`,
+  },
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
